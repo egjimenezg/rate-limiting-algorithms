5fc9785922d42a676d0f8da4e611b5457f1f8791
Gamaliel Jimenez <egjimenezg@gmail.com>
2025-09-07 18:40:00 -0600

    Add RateLimiter app

---
+++ rate_limiter/lib/rate_limiter.ex
@@ -0,0 +1,19 @@
+defmodule RateLimiter.Registry do
+
+  def child_spec(_opts) do
+    Registry.child_spec(
+      keys: :unique,
+      name: __MODULE__
+    )
+  end
+
+  def lookup(rate_limiter_id) do
+    case Registry.lookup(__MODULE__, rate_limiter_id) do
+      [{rate_limiter_pid, _rate_limiter_key}] ->
+        {:ok, rate_limiter_pid}
+      [] ->
+        {:error, :not_found}
+    end
+  end
+
+end
