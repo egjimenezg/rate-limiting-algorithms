a1c95da9be0154493f07d5eed5502df961dc5c78
Gamaliel Jimenez <egjimenezg@gmail.com>
2025-09-17 23:41:02 -0600

    feat: add docker compose file to create new service to start redis

--- rentals-api/src/rentals_api/models.py
+++ rentals-api/src/rentals_api/models.py
@@ -1,3 +1,17 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
+class Base(DeclarativeBase): pass
 
-class Listing()
+class Rental(Base):
+  __tablename__ = "rentals"
+  id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+  city: Mapped[str] = mapped_column(String(64), index=True)
+  price: Mapped[int] = mapped_column(Integer, index=True)
+
+class User(Base):
+  __tablename__ = "users"
+  id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+  email: Mapped[int] = mapped_column(String(120), unique=True, index=True)
+
